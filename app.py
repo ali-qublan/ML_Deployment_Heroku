73a92ffb635e7a17d2482c424b85cb443b2f3267
Ali Qublan <57020764+ali-qublan@users.noreply.github.com>
2021-07-05 19:46:11 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -3,13 +3,12 @@ from  flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open(r'C:\Users\aliqu\model.pkl','rb'))
-
+model = pickle.load(open('model.pkl','rb'))
 
 
 @app.route('/')
 def home():
-    render_template('index.html')
+    return render_template('index.html')
     
 @app.route('/predict',methods=['POST'])
 def predict():
